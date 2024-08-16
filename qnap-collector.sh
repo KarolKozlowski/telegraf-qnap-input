b9380b521c3dfbe928d249fdd89d5580ae57f90a
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2024-08-16 10:07:41 +0200

    Optimize loops

--- qnap-collector.sh
+++ qnap-collector.sh
@@ -31,7 +31,7 @@ systmp_unit=$(getsysinfo systmp | cut -d "/" -f1 | cut -d " " -f2) # get system 
 system_version=$(getcfg system version)
 
 sysfannum=$(getsysinfo sysfannum) # get total system fan number
-for fan_no in  $(seq 1 ${sysfannum}); do
+for (( fan_no=0; fan_no<${sysfannum}; fan_no++ ));  do
   sysfan=$(getsysinfo sysfan ${fan_no} | cut -d " " -f1 ) # get system fan speed
   sysfields="${sysfields}fan_${fan_no}=${sysfan},"
 done
@@ -40,7 +40,7 @@ echo "sys ${sysfields}tmp=${systmp},tmp_unit=\"${systmp_unit}\",version=\"${syst
 
 
 hdnum=$(getsysinfo hdnum) # get total system SATA disk number
-for disk_no in $(seq 1 ${hdnum}); do
+for (( disk_no=1; disk_no<=${hdnum}; disk_no++ ));  do
   hdtmp=$(getsysinfo hdtmp ${disk_no}| cut -d "/" -f1 | cut -d " " -f1) # get SATA disk temperature
   hdtmp_unit=$(getsysinfo hdtmp ${disk_no}| cut -d "/" -f1 | cut -d " " -f2) # get SATA disk temperature
   hdstatus=$(getsysinfo hdstatus ${disk_no}) # get SATA disk status
@@ -59,7 +59,7 @@ done
 
 
 sysvolnum=$(getsysinfo sysvolnum) # get system volume number
-for volume_no in $(seq 0 $((${sysvolnum}-1))); do
+for (( volume_no=0; volume_no<${sysvolnum}; volume_no++ ));  do
   vol_desc=$(getsysinfo vol_desc ${volume_no}) # get volume description
   vol_fs=$(getsysinfo vol_fs ${volume_no}) # get volume file system
   vol_totalsize=$(getsysinfo vol_totalsize ${volume_no} | cut -d " " -f1) # get volume total size
